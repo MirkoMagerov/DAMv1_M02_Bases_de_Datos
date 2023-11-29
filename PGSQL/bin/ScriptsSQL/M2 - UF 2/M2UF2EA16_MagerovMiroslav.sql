a8bef025b6ea528809aac820924ffa34528fd847
miroslav.magerov <miroslav.magerov@gmail.com>
2023-11-30 00:31:53 +0100

    EA 16 Acabado

--- PGSQL/bin/ScriptsSQL/M2 - UF 2/M2UF2EA16_MagerovMiroslav.sql
+++ PGSQL/bin/ScriptsSQL/M2 - UF 2/M2UF2EA16_MagerovMiroslav.sql
@@ -2,12 +2,16 @@
 Mostra el codi de destí, la ciutat de destí, el NIF de la empresa productora, la ciutat de l’empresa productora i el
 tractament que han fet servir les empreses productores que han traslladat la màxima quantitat (quantitat_trasllat)
 fent servir un tractament del tipus "Segregació i lliurament". */
-/* NO VA */
 SELECT d.cod_desti, d.ciutat_desti, ep.nif_empresa, ep.ciutat_empresa, t.tractament
 FROM desti d
-JOIN trasllat t USING (cod_desti)
-JOIN empresaproductora ep USING (nif_empresa)
-WHERE LOWER(t.tractament) LIKE 'segregacio i lliurament';
+JOIN trasllat t ON d.cod_desti = t.cod_desti
+JOIN empresaproductora ep ON t.nif_empresa = ep.nif_empresa
+WHERE t.nif_empresa IN (
+    SELECT ep2.nif_empresa
+    FROM empresaproductora ep2
+    WHERE t.quantitat_trasllat >= ALL (
+        SELECT MAX(t2.quantitat_trasllat)
+        FROM trasllat t2));
 
 /* Exercici 2 BBDD residus
 Quin tipus de tractament s'ha fet servir menys durant l'any 2017 (utilitzarem la data d'enviament per comptar el
@@ -39,8 +43,7 @@ LIMIT 2;
 
 /* Exercici 4 BBDD residus
 Obtenir el nom de totes les ciutats a les que van a parar els residus que inclouen el constituent "Solucions
-bàsiques o bases en forma sòlid". Mostra els resultats ordenats per ciutat destí. Utilitza només subconsultes. */
- /* NO VA */
+bàsiques o bases en forma sòlida". Mostra els resultats ordenats per ciutat destí. Utilitza només subconsultes. */
 SELECT d.ciutat_desti
 FROM desti d
 WHERE d.cod_desti IN (
@@ -52,7 +55,7 @@ WHERE d.cod_desti IN (
         WHERE rc.cod_constituent IN (
             SELECT c.cod_constituent
             FROM constituent c
-            WHERE LOWER(c.nom_constituent) LIKE 'solucions bàsiques o bases en forma sòlid'
+            WHERE LOWER(c.nom_constituent) LIKE 'solucions bàsiques o bases en forma sòlida'
         )
     )
 )
@@ -61,31 +64,44 @@ ORDER BY d.ciutat_desti;
 /* Exercici 5 BBDD training
 Mostrar l'identificador del client i el nom de l’empresa client dels clients on el límit de crèdit sigui més petit que
 la suma de tots els imports de les comandes d'aquell client. Utilitza la subconsulta i no es pot agrupar. */
-/* NO VA */
 SELECT c.num_clie, c.empresa
 FROM clientes c
-WHERE c.num_clie IN (
-    SELECT p.clie
+WHERE c.limite_credito < (
+    SELECT SUM(p.importe)
     FROM pedidos p
-    WHERE c.limite_credito < p.importe);
+    WHERE p.clie = c.num_clie
+    );
 
 /* Exercici 6 BBDD training
 Mostrar tots els camps d'aquelles oficines que tots els seus venedors tinguin unes vendes superiors a la suma dels
 imports de totes les comandes. Utilitza subconsultes. */
 SELECT o.*
-FROM oficinas o
-JOIN repventas rv ON o.oficina = rv.oficina_rep
+FROM oficinas o, pedidos p
 GROUP BY o.oficina
-HAVING SUM(rv.ventas) > (
-    SELECT SUM(p.importe)
-    FROM pedidos p, repventas rv2
-    WHERE rv2.num_empl = p.rep );
+HAVING SUM(p.importe) <ALL (
+    SELECT ventas
+    FROM repventas rp
+    WHERE o.oficina = rp.oficina_rep);
 
 /* Exercici 7 BBDD training
 Mostra el nom de l'empresa client, l'identificador i el nom del venedor assignat al client, i l’identificador i la
 ciutat de l'oficina en la que treballa el venedor assignat al client en cas que aquest tingui una oficina assignada. */
-
+SELECT c.empresa, c.num_clie, rp.nombre, rp.num_empl, o.ciudad
+FROM clientes c
+JOIN repventas rp ON c.rep_clie = rp.num_empl
+JOIN oficinas o ON rp.oficina_rep = o.oficina;
 
 /* Exercici 8 BBDD training
 Revisa aquest exemple d’utilització de l’expressió condicional CASE, similar a IF/ELSE en altres llenguatges de
 programació, en una consulta a la base de dades HR: */
+SELECT COUNT(p.num_pedido),
+CASE 
+    WHEN COUNT(p.num_pedido) > THEN 'Gran Client'
+    WHEN COUNT(p.num_pedido) >= 5 AND COUNT(p.num_pedido) <= 10 THEN 'Client mitja'
+    ELSE 'Petit Client'
+END
+FROM pedidos p
+WHERE p.clie = (
+    SELECT c.num_clie
+    FROM clientes c
+    WHERE LOWER(c.empresa) LIKE 'zetacorp');
